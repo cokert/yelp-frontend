1102b8d0d55895ade466d1d02572d6a8981b79e5
Tim Coker <cokert@gmail.com>
2022-03-20 01:41:55 -0400

    added geolocation from ip

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { DebounceInput } from 'react-debounce-input';
+import React, { useState, useEffect } from "react";
+import { DebounceInput } from "react-debounce-input";
+import publicIp from "public-ip";
 
 // bootstrap things
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Jumbotron, Form, FormLabel } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Form,
+  FormLabel,
+} from "react-bootstrap";
 
 //custom components
-import Display from './components/display.js';
-import Error from './components/error.js';
-import './app.css';
-import locateIcon from './location.png';
+import Display from "./components/display.js";
+import Error from "./components/error.js";
+import "./app.css";
+import locateIcon from "./location.png";
 
 const debounceTimeout = 500;
 
@@ -17,48 +25,102 @@ async function doSearch(searchParams) {
   const { term, location, limit } = searchParams;
 
   try {
-    let res = await fetch(`/api/search?term=${term}&location=${location}&limit=${limit}`)
+    let res = await fetch(
+      `/api/search?term=${term}&location=${location}&limit=${limit}`
+    );
     let data = await res.json();
     return { data: data };
   } catch (e) {
-    return ({});
+    return {};
   }
 }
 
+const logerr = (err, msg) => {
+  if (!!msg) {
+    console.error(msg, err);
+  } else {
+    console.error(err);
+  }
+};
+
 export default function App() {
   const [data, setData] = useState({});
-  const [searchParams, setSearchParams] = useState({ term: 'Ice Cream', location: 'Alpharetta, GA', limit: 5 });
+  const [searchParams, setSearchParams] = useState({
+    term: "Burger",
+    location: undefined,
+    limit: 5,
+  });
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    async function fetch() {
-      const searchRes = await doSearch(searchParams);
-      setData(searchRes.data);
+    fetch(`https://ipapi.co/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("geores", data);
+        const location = `${data?.city}, ${data?.region}`;
+        setSearchParams({
+          ...searchParams,
+          location,
+        });
+        locateMe();
+      })
+      .catch(logerr);
+  }, []);
+
+  useEffect(() => {
+    if (!!searchParams.location) {
+      const params = { ...searchParams };
+      if (!!location) {
+        params.location = location;
+      }
+      doSearch(params).then((res) => setData(res.data));
     }
-    fetch();
-  }, [searchParams]);
+  }, [searchParams, location]);
 
   //this function sets the search parameter specified in name and perists the others.  since search data is one object
   //with one set function, they have to be set together.  this function also allows us to perform some validation,
   //like limiting the number of rows returned to 50 to avoid a 400 from the yelp API and also set the data to null
   //so "Loading.." is shown while the search is performed.
   function setValue(name, value) {
-    let vals = { term: searchParams.term, location: searchParams.location, limit: searchParams.limit };
+    let vals = {
+      term: searchParams.term,
+      location: searchParams.location,
+      limit: searchParams.limit,
+    };
     vals[name] = value;
     setData(null);
     if (name === "limit" && value > 51) {
       //api has limit of 50
       vals["limit"] = 50;
     }
+    if (name === "location") {
+      setLocation(undefined);
+    }
     setSearchParams(vals);
   }
 
+  const updateLocation = (location) => {
+    setLocation(location);
+    setSearchParams({
+      ...searchParams,
+      location: "Your Current Location",
+    });
+  };
+
   function locateMe() {
-    console.log('locating...');
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      let location = "coords: " + position.coords.latitude + " " + position.coords.longitude;
-      setValue('location', location)
-    }, (error) => console.log(`error getting location: ${error}`));
+    console.log("locating...");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        let location =
+          "coords: " +
+          position.coords.latitude +
+          " " +
+          position.coords.longitude;
+        updateLocation(location);
+      },
+      (error) => console.log(`error getting location: ${error}`)
+    );
   }
 
   return (
@@ -71,33 +133,55 @@ export default function App() {
       <Form>
         <Row className="mb-3">
           <Col>
-            <FormLabel>
-              Search
-            </FormLabel>
-            <DebounceInput className="form-control" debounceTimeout={debounceTimeout} value={searchParams.term} onChange={(e) => setValue("term", e.target.value)}></DebounceInput>
+            <FormLabel>Search</FormLabel>
+            <DebounceInput
+              className="form-control"
+              debounceTimeout={debounceTimeout}
+              value={searchParams.term}
+              onChange={(e) => setValue("term", e.target.value)}
+            ></DebounceInput>
           </Col>
           <Col>
             <FormLabel>
-              Location<a onClick={locateMe}><img src={locateIcon} className='img-fluid' style={{ height: '20px' }} alt='Locate Me' /><small>Click to use current location</small></a>
+              Location
+              <a onClick={locateMe}>
+                <img
+                  src={locateIcon}
+                  className="img-fluid"
+                  style={{ height: "20px" }}
+                  alt="Locate Me"
+                />
+                <small>Click to use current location</small>
+              </a>
             </FormLabel>
-            <DebounceInput className="form-control" debounceTimeout={debounceTimeout} value={searchParams.location} onChange={(e) => setValue("location", e.target.value)}></DebounceInput>
+            <DebounceInput
+              className="form-control"
+              debounceTimeout={debounceTimeout}
+              value={searchParams.location}
+              onChange={(e) => setValue("location", e.target.value)}
+            ></DebounceInput>
           </Col>
           <Col>
             <FormLabel>
               Results to return <small>The max is 50</small>
             </FormLabel>
-            <DebounceInput className="form-control" debounceTimeout={debounceTimeout} value={searchParams.limit} onChange={(e) => setValue("limit", e.target.value)}></DebounceInput>
+            <DebounceInput
+              className="form-control"
+              debounceTimeout={debounceTimeout}
+              value={searchParams.limit}
+              onChange={(e) => setValue("limit", e.target.value)}
+            ></DebounceInput>
           </Col>
         </Row>
       </Form>
-      {data ?
+      {data ? (
         <>
           {data.businesses && <Display data={data} search={searchParams} />}
           {data.error && <Error error={data.error} />}
         </>
-        :
+      ) : (
         <h1>Loading...</h1>
-      }
+      )}
     </Container>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,76 +1,82 @@
-const express = require('express'),
-      bent = require('bent'),
-      fs  = require('fs'),
-      https = require('https');
-
+const express = require("express"),
+  bent = require("bent"),
+  fs = require("fs"),
+  https = require("https");
 
 const app = express();
 
 let certOptions = undefined;
 
 try {
-    certOptions = {
-        key: fs.readFileSync('/certs/privkey1.pem').toString(),
-        cert: fs.readFileSync('/certs/cert1.pem').toString()
-    }
+  certOptions = {
+    key: fs.readFileSync("/certs/privkey1.pem").toString(),
+    cert: fs.readFileSync("/certs/cert1.pem").toString(),
+  };
 } catch (e) {
-    console.log('https certs not found in /certs (or load error), https will be unavailable:', e.message);
+  console.log(
+    "https certs not found in /certs (or load error), https will be unavailable:",
+    e.message
+  );
 }
 
 const APIKEY = process.env.API_KEY;
 
 if (!APIKEY) {
-    console.log("API_KEY not set.  Set as environment var or start with `API_KEY=<value> npm run dev`. Exiting...");
-    process.exit(1);
+  console.log(
+    "API_KEY not set.  Set as environment var or start with `API_KEY=<value> npm run dev`. Exiting..."
+  );
+  process.exit(1);
 }
 const headers = {
-    'Authorization': 'Bearer ' + APIKEY,
+  Authorization: "Bearer " + APIKEY,
 };
-const getStream = bent('https://api.yelp.com', headers);
+const getStream = bent("https://api.yelp.com", headers);
 
 app.use((req, res, next) => {
-    if (certOptions) {
-        //redirect to secure if https is available
-        console.log('middle ware triggered!');
-        if (!req.secure ) {
-            res.redirect (301, `https://${req.hostname}:443${req.originalUrl}`);
-        } else {
-            next();
-        }
+  if (certOptions) {
+    //redirect to secure if https is available
+    console.log("middle ware triggered!");
+    if (!req.secure) {
+      res.redirect(301, `https://${req.hostname}:443${req.originalUrl}`);
     } else {
-        next();
+      next();
     }
+  } else {
+    next();
+  }
 });
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/api/search', async (req, res) => {
-    const {term, location, limit} = req.query;
-    try {
-        let url = `/v3/businesses/search?term=${term}&sort_by=rating&limit=${limit}`
-        if (location.startsWith('coords')) {
-            const lat = location.split(' ')[1];
-            const long = location.split(' ')[2];
-            url += `&latitude=${lat}&longitude=${long}`;
-        } else {
-            url += `&location=${location}`;
-        }
-        let stream = await getStream(url, null, headers);
-        const obj = await stream.json();
-        res.send(obj);
-    } catch (error) {
-        console.log('api request error', error);
-        if (error.statusCode) {
-            res.statusCode = error.statusCode;
-        } else {
-            res.statusCode = 500;
-        }
-        res.send({error: error});
-    }    
+app.get("/api/search", async (req, res) => {
+  const { term, location, limit } = req.query;
+  try {
+    let url = `/v3/businesses/search?term=${term}&sort_by=rating&limit=${limit}`;
+    if (location.startsWith("coords")) {
+      const lat = location.split(" ")[1];
+      const long = location.split(" ")[2];
+      url += `&latitude=${lat}&longitude=${long}`;
+    } else {
+      url += `&location=${location}`;
+    }
+    let stream = await getStream(url, null, headers);
+    const obj = await stream.json();
+    res.send(obj);
+  } catch (error) {
+    console.log("api request error", error);
+    if (error.statusCode) {
+      res.statusCode = error.statusCode;
+    } else {
+      res.statusCode = 500;
+    }
+    res.send({ error: error });
+  }
 });
 const port = process.env.PORT || 8080;
 
 if (certOptions) {
-    https.createServer(certOptions, app).listen(443, () => console.log(`HTTPS listening on port 443!`));
+  https
+    .createServer(certOptions, app)
+    .listen(443, () => console.log(`HTTPS listening on port 443!`));
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
